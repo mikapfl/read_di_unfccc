c27ebd1a69db272d45ec63ddf3e9945c055c9849
floriandierickx <floriandierickx@gmail.com>
2020-10-19 23:18:25 +0200

    added root folder reference in download script

--- download_all.py
+++ download_all.py
@@ -1,15 +1,17 @@
 import tqdm
+import os
 import pandas as pd
 
 from read_di_unfccc import UNFCCCApiReader
 
+ROOT_DIR = os.path.abspath(os.curdir) # This is your Project Root
 
 def main():
     r = UNFCCCApiReader()
     for party in tqdm.tqdm(r.parties["code"], desc="parties"):
         df = r.query(party_code=party, progress=True)
-        df.to_csv(f"outputs/{party}.csv.gz", compression="gzip")
-        df.to_parquet(f"outputs/{party}.parquet")
+        df.to_csv(ROOT_DIR + '//' + f"outputs/{party}.csv.gz", compression="gzip")
+        df.to_parquet(ROOT_DIR + '//' + f"outputs/{party}.parquet")
 
 
 if __name__ == "__main__":
