56d7c342ac2f0bec4eee108f29e602b42a39db5b
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2020-10-20 11:27:27 +0200

    Download script: OS-agnostic path manipulation

--- download_all.py
+++ download_all.py
@@ -1,17 +1,19 @@
 import tqdm
 import os
 import pandas as pd
+import pathlib
 
 from read_di_unfccc import UNFCCCApiReader
 
-ROOT_DIR = os.path.abspath(os.curdir) # This is your Project Root
+ROOT_DIR = pathlib.Path(os.path.abspath(os.curdir))  # This is your Project Root
+
 
 def main():
     r = UNFCCCApiReader()
     for party in tqdm.tqdm(r.parties["code"], desc="parties"):
         df = r.query(party_code=party, progress=True)
-        df.to_csv(ROOT_DIR + '//' + f"outputs/{party}.csv.gz", compression="gzip")
-        df.to_parquet(ROOT_DIR + '//' + f"outputs/{party}.parquet")
+        df.to_csv(ROOT_DIR / "outputs" / f"{party}.csv.gz", compression="gzip")
+        df.to_parquet(ROOT_DIR / "outputs" / f"{party}.parquet")
 
 
 if __name__ == "__main__":
